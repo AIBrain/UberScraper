208be7dc3e61310c09cd3952e5bc5d81455eb81f
AIBrain <rick@aibrain.org>
2014-09-22 14:43:03 -0600

    Resharper people.. RESHARPER!

--- UberScraper/Uber.cs
+++ UberScraper/Uber.cs
@@ -29,7 +29,6 @@ namespace UberScraper {
     using System.ComponentModel;
     using System.Diagnostics;
     using System.Drawing;
-    using System.Drawing.Imaging;
     using System.IO;
     using System.Linq;
     using System.Threading;
